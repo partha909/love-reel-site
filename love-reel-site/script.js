ee51c68287618255b9fff3ce2564fc113280b751
partha909 <parthasahap9255@gmail.com>
2026-03-12 14:06:32 +0530

    Update script.js

--- love-reel-site/script.js
+++ love-reel-site/script.js
@@ -1,12 +1,31 @@
-function start(){
+document.getElementById("generateBtn").addEventListener("click", function(){
 
-let name=document.getElementById("nameInput").value
+const creator = document.getElementById("nameInput").value
+const lover = document.getElementById("loverInput").value
 
-if(name==""){
-alert("Enter your name")
+const musicFile = document.getElementById("music").files[0]
+const photos = document.getElementById("photos").files
+
+if(!creator || !lover || !musicFile || photos.length === 0){
+
+alert("Please fill all fields ❤️")
+
 return
-}
-
-alert("Video made by "+name)
 
 }
+
+const id = Date.now().toString(36)
+
+const link =
+window.location.origin +
+"/view.html?id=" + id +
+"&creator=" + encodeURIComponent(creator) +
+"&lover=" + encodeURIComponent(lover)
+
+document.getElementById("generatedLink").innerText = link
+
+navigator.clipboard.writeText(link)
+
+alert("Love link created & copied ❤️")
+
+})
