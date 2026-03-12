3721b9565973e92161989c3be6f50408b55d9737
partha909 <parthasahap9255@gmail.com>
2026-03-12 14:30:32 +0530

    Update script.js

--- love-reel-site/script.js
+++ love-reel-site/script.js
@@ -3,18 +3,45 @@ document.getElementById("generateBtn").addEventListener("click", function(){
 const creator = document.getElementById("nameInput").value
 const lover = document.getElementById("loverInput").value
 
-if(!creator || !lover){
-alert("Please enter names ❤️")
+const musicFile = document.getElementById("music").files[0]
+const photoFiles = document.getElementById("photos").files
+
+if(!creator || !lover || !musicFile || photoFiles.length==0){
+alert("Fill all fields ❤️")
 return
 }
 
-const id = Date.now().toString(36)
+const readerMusic = new FileReader()
+
+readerMusic.onload=function(){
+
+localStorage.setItem("musicData",readerMusic.result)
+
+}
+
+readerMusic.readAsDataURL(musicFile)
+
+let photos=[]
+
+Array.from(photoFiles).forEach(file=>{
+
+const reader=new FileReader()
+
+reader.onload=function(){
+
+photos.push(reader.result)
+
+localStorage.setItem("photoData",JSON.stringify(photos))
+
+}
+
+reader.readAsDataURL(file)
+
+})
 
 const link =
 window.location.origin +
-"/view.html?id=" + id +
-"&creator=" + encodeURIComponent(creator) +
-"&lover=" + encodeURIComponent(lover)
+"/view.html?creator=" + encodeURIComponent(creator)
 
 document.getElementById("generatedLink").innerText = link
 
